e00dffe202030588d86897993f9df9a3e8745080
Morior <moriorgames@gmail.com>
2018-02-17 18:47:58 +0100

    Try with ncurses

--- main.cpp
+++ main.cpp
@@ -1,23 +1,59 @@
-#include <iostream>
+#include <ncurses.h>
 
-#define ANSI_BLOCK          "\u2588"
-#define ANSI_COLOR_RED      "\x1b[31m"
-#define ANSI_COLOR_GREEN    "\x1b[32m"
-#define ANSI_COLOR_YELLOW   "\x1b[33m"
-#define ANSI_COLOR_BLUE     "\x1b[34m"
-#define ANSI_COLOR_MAGENTA  "\x1b[35m"
-#define ANSI_COLOR_CYAN     "\x1b[36m"
-#define ANSI_COLOR_RESET    "\x1b[0m"
-
-using namespace std;
+#define ANSI_BLOCK      "\u2588"
+#define ANSI_LIGHT      "\u2591"
+#define ANSI_MEDIUM     "\u2592"
+#define ANSI_DARK       "\u2593"
 
 int main()
 {
-    cout << ANSI_COLOR_GREEN;
-    for (int i = 1; i <= 30; ++i) {
-        cout << ANSI_BLOCK;
+//    initscr();
+//    start_color();
+//
+//    init_pair(1, COLOR_BLACK, COLOR_RED);
+//    init_pair(2, COLOR_BLACK, COLOR_GREEN);
+//
+//    attron(COLOR_PAIR(1));
+//    printw("This should be printed in black with a red background!\n");
+//
+//    attron(COLOR_PAIR(2));
+//    printw("And this in a green background!\n");
+//    refresh();
+//
+//    getch();
+//
+//    endwin();
+
+
+
+    int lines = 40, cols = 120, y0 = 0, x0 = 0;
+    bool finished = false;
+    initscr();
+    WINDOW *win = newwin(lines, cols, y0, x0);
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_GREEN);
+
+    while (!finished) {
+
+        auto keyInput = getch();
+        if (keyInput == ERR) {
+            continue;         /* ignore when there was a timeout - no data */
+        } else if (keyInput == 'q' || keyInput == 'Q') {
+            finished = true;
+        }
+//        attron(COLOR_PAIR(1));
+        for (int i = 0; i < lines; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                mvwaddch(win, i, j, ACS_DIAMOND | COLOR_PAIR(1));
+            }
+        }
+//        attroff(COLOR_PAIR(1));
+
+        wrefresh(win);
     }
-    cout << ANSI_COLOR_RESET << endl;
+
+    endwin();
 
     return 0;
 }
+
